a293e0fd82f0753535b48cbb77b6359d475051e1
13ogrammer <tasker.prabhat@gmail.com>
2024-08-19 18:05:10 +1000

    highlight currently selected navigation item

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,27 @@
+'use client'
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const siteRoutes = [
+  { href: "/", label: "Home" },
+  { href: "/tasks", label: "Tasks" },
+  { href: "/notes", label: "Notes" }
+]
 
 export default function Header() {
+  const pathname = usePathname();
+
   return <header className="flex justify-between items-center py-4 px-7 border-b">
     <h1 className="font-bold text-lg">Self Dev Planner</h1>
-    <nav className="flex gap-4">
-      <Link href="/">Home</Link>
-      <Link href="/tasks">Tasks</Link>
-      <Link href="/notes">Notes</Link>
-    </nav>
+    <ul className="flex gap-4">
+      {siteRoutes.map((route) => (
+        <li key={route.href}>
+          <Link href={route.href} className={`text-zinc-400 transition ${pathname === route.href ? "text-zinc-900" : ""}`}>
+            {route.label}
+          </Link>
+        </li>
+      ))}
+    </ul>
   </header>
 }
